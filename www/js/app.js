047b07adada0ba3baaf84360d4ac84aefa1dc587
freejoe76 <jmurphy@denverpost.com>
2017-12-18 15:51:05 -0500

    kill ds store, more object work

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,7 @@ const second = 1000,
 	day = hour * 24;
 
 counts = {
+	target_time: null,
     config: {
 		direction: 0, 	// 0 to count down, 1 to count up
     },
@@ -20,10 +21,13 @@ counts = {
     init: {
         // Config handling. External config objects must be named c_config
         if ( typeof c_config !== 'undefined' ) this.update_config(c_config);
+		if ( typeof target_time !== 'undefined') this.config['target_time'] = target_time;
+		this.target_time = new Date(this.config['target_time']).getTime();
+
 		let x = setInterval (function() {
 
 			let now = new Date().getTime(),
-			distance = countDown - now;
+			let distance = counts.target_time - now;
 
 			document.getElementById('days').innerHTML = Math.floor(distance / (day)),
 			document.getElementById('hours').innerHTML = Math.floor((distance % (day)) / (hour)),
