5fdb0a841dfd5e8c0b1753fb0b2ad814bb32c19b
freejoe76 <jmurphy@denverpost.com>
2017-12-18 11:48:18 -0500

    Improve title, add more information and links to the page

--- www/js/app.js
+++ www/js/app.js
@@ -20,18 +20,18 @@ counts = {
     init: {
         // Config handling. External config objects must be named c_config
         if ( typeof c_config !== 'undefined' ) this.update_config(c_config);
+		let x = setInterval (function() {
+
+			let now = new Date().getTime(),
+			distance = countDown - now;
+
+			document.getElementById('days').innerHTML = Math.floor(distance / (day)),
+			document.getElementById('hours').innerHTML = Math.floor((distance % (day)) / (hour)),
+			document.getElementById('minutes').innerHTML = Math.floor((distance % (hour)) / (minute)),
+			document.getElementById('seconds').innerHTML = Math.floor((distance % (minute)) / second);
+
+		}, second)
     }
 };
 
 let countDown = new Date('May 19, 2018 00:00:00').getTime(),
-	x = setInterval (function() {
-
-		let now = new Date().getTime(),
-		distance = countDown - now;
-
-		document.getElementById('days').innerHTML = Math.floor(distance / (day)),
-		document.getElementById('hours').innerHTML = Math.floor((distance % (day)) / (hour)),
-		document.getElementById('minutes').innerHTML = Math.floor((distance % (hour)) / (minute)),
-		document.getElementById('seconds').innerHTML = Math.floor((distance % (minute)) / second);
-
-	}, second)
