59cc5f21ebdfed30be79f6ea21ab239ec75b5b94
freejoe76 <jmurphy@denverpost.com>
2018-02-11 23:22:22 -0500

    Update time for royal wedding

--- www/royal-wedding-prince-harry-meghan-markle/countdown.js
+++ www/royal-wedding-prince-harry-meghan-markle/countdown.js
@@ -3,7 +3,7 @@ const second = 1000,
 	hour = minute * 60,
 	day = hour * 24;
 
-let countDown = new Date('May 19, 2018 09:00:00').getTime(),
+let countDown = new Date('May 19, 2018 07:00:00').getTime(),
 	x = setInterval (function() {
 
 		let now = new Date().getTime(),
